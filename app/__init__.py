f92b9b3e89c561487415cff30b7b7da60c6f610c
Taty <tatyanaparks@gmail.com>
2022-08-12 18:39:02 -0500

    edit

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
-
+#
 
 #init my login manager
 login = LoginManager()
